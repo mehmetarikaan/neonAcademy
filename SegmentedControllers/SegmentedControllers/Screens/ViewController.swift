ccb16a2cb07af30997dbcdd807675ffeebdd2995
Mehmet Arikan <mehmetarikaan@outlook.com>
2023-03-06 09:36:50 +0300

    segmented control task is edited

--- SegmentedControllers/SegmentedControllers/Screens/ViewController.swift
+++ SegmentedControllers/SegmentedControllers/Screens/ViewController.swift
@@ -11,62 +11,65 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    lazy var segmentControll: UISegmentedControl = {
-        let segment = UISegmentedControl(items: ["Neon Academy 2023", "Neon", "Apps"])
-        segment.backgroundColor = .systemCyan
-        segment.tintColor = .white
-        segment.selectedSegmentIndex = 0
-        segment.addTarget(self, action: #selector(segmentAction(_:)), for: .valueChanged)
-        view.backgroundColor = .systemPink
-        return segment
-    }()
+    
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        setupUIDesign()
     }
-    
-    func setupUI(){
-        view.addSubview(segmentControll)
-        
-        segmentControll.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(80)
-            make.trailing.equalToSuperview().inset(20)
-            make.leading.equalToSuperview().offset(20)
-            make.centerX.equalTo(view.center)
+    func setupUIDesign() {
+        let segmentedControl: UISegmentedControl = {
+            let segmented = UISegmentedControl(items: ["Neon Academy 2023", "Neon", "Apps"])
+            segmented.selectedSegmentIndex = 0
+            segmented.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.black], for: .normal)
+            segmented.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.purple], for: .selected)
+            segmented.tintColor = .red
+            segmented.translatesAutoresizingMaskIntoConstraints = false
+            segmented.addTarget(self, action: #selector(handleSegmentChange), for: .valueChanged)
+            segmented.isMomentary = false
+            return segmented
+        }()
+        view.addSubview(segmentedControl)
+        segmentedControl.backgroundColor = .systemGray4
+        segmentedControl.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(40)
         }
     }
     
-    @objc func segmentAction(_ sender: UISegmentedControl){
+    @objc func handleSegmentChange(sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
-            segmentControll.snp.makeConstraints { make in
-                make.height.equalTo(300)
-                make.width.equalToSuperview()
-            }
-            view.backgroundColor = .systemPink
+            sender.frame.size.width = 200
+            sender.selectedSegmentTintColor = .systemPurple
+            view.backgroundColor = .white
+            sender.setWidth(screenWidth/3, forSegmentAt: 0)
+            sender.setWidth(screenWidth/4, forSegmentAt: 1)
+            sender.setWidth(screenWidth/4, forSegmentAt: 2)
+            
         case 1:
-            segmentControll.snp.makeConstraints { make in
-                make.height.equalTo(80)
-                make.width.equalToSuperview()
-            }
-            view.backgroundColor = .systemGreen
+            sender.frame.size.width = 200
+            sender.selectedSegmentTintColor = .systemGreen
+            view.backgroundColor = .systemPurple
+            sender.setWidth(screenWidth/3.5, forSegmentAt: 1)
+            sender.setWidth(screenWidth/4, forSegmentAt: 0)
+            sender.setWidth(screenWidth/4, forSegmentAt: 2)
         case 2:
-            segmentControll.snp.makeConstraints { make in
-                make.height.equalTo(60)
-                make.width.equalToSuperview()
-            }
-            view.backgroundColor = .systemRed
+            sender.frame.size.width = 200
+            sender.selectedSegmentTintColor = .systemCyan
+            view.backgroundColor = .green
+            sender.setWidth(screenWidth/3.9, forSegmentAt: 2)
+            sender.setWidth(screenWidth/4, forSegmentAt: 0)
+            sender.setWidth(screenWidth/4, forSegmentAt: 1)
         default:
-            segmentControll.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(80)
-                make.trailing.equalToSuperview().inset(20)
-                make.leading.equalToSuperview().offset(20)
-                make.centerX.equalTo(view.center)
-            }
-            view.backgroundColor = .systemRed
+            
+            break
         }
     }
-    
-    
 }
+
 
