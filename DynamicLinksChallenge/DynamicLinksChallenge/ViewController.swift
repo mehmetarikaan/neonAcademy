68cf21c3d8229c22e6cac49c85a8072222f4fd4c
Mehmet Arikan <mehmetarikaan@outlook.com>
2023-03-14 13:16:16 +0300

    dynamic link task is completed

---
+++ DynamicLinksChallenge/DynamicLinksChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicLinksChallenge
+//
+//  Created by Mehmet Arıkan on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
