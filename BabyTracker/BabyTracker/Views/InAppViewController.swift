f4eb199e81c299545b693a5d4207acd45470ca12
Mehmet Arikan <mehmetarikaan@outlook.com>
2023-03-14 17:52:54 +0300

    inappvc task is editing

--- BabyTracker/BabyTracker/Views/InAppViewController.swift
+++ BabyTracker/BabyTracker/Views/InAppViewController.swift
@@ -33,6 +33,49 @@ class InAppViewController: UIViewController {
         return image
     }()
     
+    lazy var imageDotOne: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "icon_matter")
+        return image
+    }()
+    lazy var labelOne: UILabel = {
+       let label = UILabel()
+        label.text = "Share the care"
+        label.numberOfLines = 0
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textAlignment = .left
+        return label
+    }()
+    lazy var imageDotTwo: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "icon_matter")
+        return image
+    }()
+    lazy var labelTwo: UILabel = {
+       let label = UILabel()
+        label.text = "All-in-one baby tracker"
+        label.numberOfLines = 0
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textAlignment = .left
+        return label
+    }()
+    lazy var imageDotThree: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "icon_matter")
+        return image
+    }()
+    lazy var labelThree: UILabel = {
+       let label = UILabel()
+        label.text = "Watch your babys growth"
+        label.numberOfLines = 0
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textAlignment = .left
+        return label
+    }()
+    
     //MARK: - Lifeycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,6 +98,34 @@ class InAppViewController: UIViewController {
         imageView.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(28)
             make.centerX.equalToSuperview()
+        }
+        
+        let stackOne = UIStackView(arrangedSubviews: [imageDotOne, labelOne])
+        stackOne.axis = .horizontal
+        stackOne.spacing = -10
+        
+        let stackTwo = UIStackView(arrangedSubviews: [imageDotTwo, labelTwo])
+        stackTwo.axis = .horizontal
+        stackTwo.spacing = -10
+        
+        let stackThree = UIStackView(arrangedSubviews: [imageDotThree, labelThree])
+        stackThree.axis = .horizontal
+        stackThree.spacing = -10
+        
+        view.addSubview(stackOne)
+        stackOne.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(32)
+            make.leading.equalTo(109)
+        }
+        view.addSubview(stackTwo)
+        stackTwo.snp.makeConstraints { make in
+            make.top.equalTo(stackOne.snp.top).offset(28)
+            make.leading.equalTo(109)
+        }
+        view.addSubview(stackThree)
+        stackThree.snp.makeConstraints { make in
+            make.top.equalTo(stackTwo.snp.top).offset(28)
+            make.leading.equalTo(109)
         }
     }
 }
--- RevenueChallenge/RevenueChallenge/AppDelegate.swift
+++ RevenueChallenge/RevenueChallenge/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Purchases.configure(withAPIKey: "appl_PtMQBpipMutamNKqtPAOvtwknwD", appUserID: "mehmet")
+        Purchases.configure(withAPIKey: "appl_PtMQBpipMutamNKqtPAOvtwknwD", appUserID: "mehmett")
         Purchases.debugLogsEnabled = false
         
         
